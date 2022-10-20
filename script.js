b5dd7f7e2928ac88b65421a465f035eaccd6fde1
Dasago250 <Dasago250@gmail.com>
2022-10-20 11:27:04 -0500

    Add Player choice case insensitive.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Computer decision
-function computerDecision() {
-  let decision = Math.floor(Math.random() * 3);
-  switch (decision) {
+function ComputerDecision() {
+  let computerChoice = Math.floor(Math.random() * 3);
+  switch (computerChoice) {
     case 0:
       return "Rock";
     case 1:
@@ -11,5 +11,23 @@ function computerDecision() {
   }
 }
 // Player decision
+function PlayerDecision() {
+  let regexChoice = /ROCK|PAPER|SCISSORS/gi;
+  let correct = false;
+  let playerChoice = "";
+  while (!correct) {
+    playerChoice = prompt("Rock , Paper , Scissors?")
+    if (regexChoice.test(playerChoice)){
+      correct = true;
+      break;
+    }
+    alert("Wrong Choice")
+  }
+  playerChoice = playerChoice.toLowerCase()  
+  return playerChoice
+}
+
+console.log(ComputerDecision());
+console.log(PlayerDecision());
 // PlayRound
 // Game (Function)
