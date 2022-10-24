b8a8e34bb6c34240364d8854da6e93bca7cd86e3
Dasago250 <Dasago250@gmail.com>
2022-10-24 13:44:56 -0500

    Added Scores and 5 points game

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//Player INFO
+let playerTextChoice = document.querySelector('.playerChoice');
+let playerTextScore = document.querySelector('.playerScore');
+let playerScore = 0;
+//Computer INFO
+let computerTextChoice = document.querySelector('.computerChoice');
+let computerTextScore = document.querySelector('.computerScore');
+let computerScore = 0
+//Results INFO
+let roundTextResult = document.querySelector('.roundResult');
+let winner = document.querySelector('.Winner');
+
+//Know when the game is over
+let gameOn = true;
+
+
+
 // Computer decision
 function ComputerDecision() {
   //Randomize a number between 0, 1 and 2 and depending on the result each number gives a different choice.
@@ -13,36 +30,17 @@ function ComputerDecision() {
 }
 
 // Player decision
-function PlayerDecision() {
-  //Regex ensures that what the user enters is one of the valid choices
-  let regexChoice = /ROCK|PAPER|SCISSORS/gi;
-  let correct = false;
-  let playerChoice = "";
-  //only when the user enters a valid choice the while stops and returns the choice of the player
-  while (!correct) {
-    playerChoice = prompt("Rock , Paper , Scissors?")
-    if (regexChoice.test(playerChoice)){
-      correct = true;
-      break;
-    }
-    alert("Wrong Choice");
-  }
-  playerChoice = playerChoice.toLowerCase();
-  return playerChoice;
-}
-
 let choices = document.querySelectorAll('.choice')
 
 choices.forEach(choice => {
   let choiceValue = choice.getAttribute('id');
   choice.addEventListener('click', () => {
-    PlayRound(ComputerDecision(),choiceValue);
+
+    if(gameOn){
+      PlayRound(ComputerDecision(),choiceValue);     
+    }
+    
 })})
-
-let playerTextChoice = document.querySelector('.playerChoice');
-let computerTextChoice = document.querySelector('.computerChoice')
-let roundTextResult = document.querySelector('.roundResult')
-
 
 // PlayRound
 function PlayRound(ComputerDecision,PlayerDecision ) {
@@ -52,78 +50,63 @@ function PlayRound(ComputerDecision,PlayerDecision ) {
   let playerWinsRoundMessage = "Player wins round!";
   let computerWinsRoundMessage = "Computer wins round!";
   let drawRoundMessage = "This round is a draw!";
+
   //It compares each choice and gives the winner or draw depending on the case
   switch (ComputerDecision) {
     case "Rock":
       if (PlayerDecision == "Paper") {
         roundTextResult.textContent = playerWinsRoundMessage;
-        return "Player";
+        ++playerScore;
+        playerTextScore.textContent = playerScore;
       }else if (PlayerDecision == "Scissors") {
         roundTextResult.textContent = computerWinsRoundMessage;
-        return "Computer";
+        ++computerScore;
+        computerTextScore.textContent = computerScore;
       }else{
         roundTextResult.textContent = drawRoundMessage;
-        return "Draw";
       }
+      keepScores();
+      break;
     case "Paper":
       if (PlayerDecision == "Scissors") {
         roundTextResult.textContent = playerWinsRoundMessage;
-        return "Player";
+        ++playerScore;
+        playerTextScore.textContent = playerScore;
       }else if (PlayerDecision == "Rock") {
         roundTextResult.textContent = computerWinsRoundMessage;
-        return "Computer";
+        ++computerScore;
+        computerTextScore.textContent = computerScore;
       }else{
         roundTextResult.textContent = drawRoundMessage;
-        return "Draw";
       }
+      keepScores();
+      break;
     case "Scissors":
       if (PlayerDecision == "Rock") {
         roundTextResult.textContent = playerWinsRoundMessage;
-        return "Player";
+        ++playerScore;
+        playerTextScore.textContent = playerScore;
       }else if (PlayerDecision == "Paper") {
         roundTextResult.textContent = computerWinsRoundMessage;
-        return "Computer";
+        ++computerScore;
+        computerTextScore.textContent = computerScore;
       }else{
         roundTextResult.textContent = drawRoundMessage;
-        return "Draw";
       }
+      keepScores();
+      break;
   }
 }
 
-
-
-// Game (Function)
-/*
-function Game() {
-  let winnerRound = "";
-  let playerScore = 0;
-  let computerScore = 0;
-  //Keeps score of each rounds winner 
-  for (let i = 0; i < 5; i++) {
-    winnerRound = PlayRound(ComputerDecision(),PlayerDecision());
-    console.log("");
-    switch (winnerRound) {
-      case "Player":
-        playerScore++;
-        break;
-      case "Computer":
-        computerScore++;
-        break;
-      default:
-        break;
+//Check if the player or computer reach 5 points and stops the game
+function  keepScores() {
+  if (computerScore == 5 || playerScore == 5) {
+    gameOn = false
+    if (computerScore == 5) {
+      winner.textContent = "Computer Wins!"
+    }
+    if (playerScore == 5) {
+      winner.textContent = "Player Wins!"
     }
   }
-  //Compares the scores and prints the winner or in case a draw.
-  console.log(`Player's score: ${playerScore} - Computer's score: ${computerScore}\n`);
-  if (computerScore > playerScore){
-    console.log("Computer wins the game!");
-  }else if (playerScore > computerScore) {
-    console.log("Player wins the game!");
-  }else{
-    console.log("It's a draw...");
-  }
 }
-
-Game();
-
-*/
